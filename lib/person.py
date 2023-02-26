c184c438cb54c3ac3a74e0cf5e7d7ceb3df13a0a
Ziru Zou <jiyucoding@yahoo.com>
2023-02-26 06:37:37 -0500

    redo

--- lib/person.py
+++ lib/person.py
@@ -4,29 +4,39 @@ approved_jobs = ["Admin", "Customer Service", "Human Resources", "ITC", "Product
 
 class Person:
     
-    def __ini__(self, name="", job=""):
-        self._name = name
-        self._job = job
+    def __init__(self, name='J. Doe', job='Sales'):
+        self.name = name
+        self.job = job
 
-    @property
-    def name(self):
+
+    def get_name(self):
         return self._name
 
-    @name.setter
-    def name(self, name):
+
+    def set_name(self, name):
         if type(name) == str and 1 <= len(name) <= 25:
             self._name = name.title()
         else:
             print("Name must be string between 1 and 25 characters.")
 
-    @property
-    def job(self):
+    name = property(get_name, set_name)
+
+    def get_job(self):
         return self._job
 
-    @job.setter
-    def job(self, job):
+
+    def set_job(self, job):
         if job in approved_jobs:
             self._job = job
         
         else:
             print("Job must be in list of approved jobs.")
+
+    job = property(get_job, set_job)
+
+
+
+# person = Person("Steve", "CEO")
+# print(person.name)
+# Job must be in list of approved jobs.
+# Steve
